77b178867fc04ce168815a38be4ee09db6c3c9aa
ferrpp <42369367+Abudfu@users.noreply.github.com>
2025-06-01 12:19:39 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_beryllium.mk
+    $(LOCAL_DIR)/twrp_beryllium.mk \
+    $(LOCAL_DIR)/omni_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_beryllium-userdebug \
-    twrp_beryllium-eng
+    twrp_beryllium-eng \
+    omni_beryllium-userdebug \
+    omni_beryllium-eng
